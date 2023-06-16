fbdf4abca629a854fdbcc813633357102ccd7ff2
Martin Galara <mgalara@gmail.com>
2023-06-16 15:14:31 -0300

    testing

--- src/routes/controllers/testing.js
+++ src/routes/controllers/testing.js
@@ -2,11 +2,30 @@ const { Router } = require('express');
 const { Computer, User } = require('../../db.js')
 //const userExtractor = require('../middleware/userExtractor.js.js');
 
+const axios = require('axios');
+
+
 const router = Router();
 
 router.get('/', async (req, res) => {
 
-console.log("asdasd")
+    const username = '528937882136667';
+    const password = 'H-WT2Ys7_qZb_A5KD2dW-HjtBkU';
+    const basicAuth = `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`;
+
+try {
+  const response = await axios.get('https://api.cloudinary.com/v1_1/diapwgajv/resources/image', {
+    headers: {
+      Authorization: basicAuth,
+    },
+  });
+
+  console.log(response.data);
+
+  return res.json(response.data)
+  } catch (error) {
+    console.error('Error al cargar las imágenes de Cloudinary:', error);
+  }
 })
 
 module.exports = router;
