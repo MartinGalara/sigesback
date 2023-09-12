bdc8e156c285df59f5f8e2c58b59cf4c6e7404d0
Martin Galara <mgalara@gmail.com>
2023-09-12 14:27:50 -0300

    endpoint nuevo

--- src/routes/controllers/botusers.js
+++ src/routes/controllers/botusers.js
@@ -4,28 +4,40 @@ const { Botuser, User } = require('../../db.js')
 const router = Router();
 
 router.get('/', async (req, res) => {
-    const {phone} = req.query
+    const { phone } = req.query;
     try {
-        if(phone){
-            const botUser = await Botuser.findAll({
-                where:{
+        if (phone) {
+            const botUsers = await Botuser.findAll({
+                where: {
                     phone: phone
-                }
-            })
-        return res.status(200).json(botUser)
+                },
+                include: [User] // Esto realiza un join con la tabla User
+            });
+            return res.status(200).json(botUsers);
         }
-        const allBotUsers = await Botuser.findAll();
-        return res.status(200).json(allBotUsers)
+        
+        // Si no se proporciona un número de teléfono, obtén todos los Botuser con sus usuarios relacionados.
+        const allBotUsers = await Botuser.findAll({
+            include: [User] // Esto realiza un join con la tabla User
+        });
+        
+        return res.status(200).json(allBotUsers);
     } catch (error) {
-        return res.status(400).send(error.message)
+        return res.status(400).send(error.message);
     }
-    
-})
+});
 
 router.post('/', async (req, res) => {
     try {
         // Obtener los datos del cuerpo de la solicitud
-        const { name, phone, email, createUser, canSOS, adminPdf, manager, area, userId } = req.body;
+        const { name, phone, email, createUser, canSOS, adminPdf, manager, area, userId, createdBy } = req.body;
+
+        // Verificar si el usuario con userId proporcionado existe
+        const user = await User.findByPk(userId);
+
+        if (!user) {
+            return res.status(404).json({ error: 'El usuario especificado no existe' });
+        }
 
         // Crear un nuevo usuario en la tabla Botuser y establecer la relación con el userId proporcionado
         const newUser = await Botuser.create({
@@ -37,8 +49,11 @@ router.post('/', async (req, res) => {
             adminPdf,
             manager,
             area,
-            userId // Establecer la relación con el userId proporcionado
+            createdBy
         });
+
+        // Asociar el nuevo Botuser al User existente
+        await newUser.setUser(user);
 
         // Devolver una respuesta exitosa con el nuevo usuario creado
         return res.status(201).json(newUser);
