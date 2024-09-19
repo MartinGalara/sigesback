113798068d19d4126e65a9915125488a661490d0
Martin <martin.galara@macondotek.com>
2024-09-19 16:13:19 -0300

    testing

--- src/routes/controllers/testing.js
+++ src/routes/controllers/testing.js
@@ -1,34 +1,59 @@
-const { Router } = require('express');
-const { Computer, User } = require('../../db.js')
-//const userExtractor = require('../middleware/userExtractor.js.js');
-
-const dotenv = require("dotenv");
-
-dotenv.config();
-
-const axios = require('axios');
+const { Router } = require("express");
+const { Botticket } = require("../../db.js");
+const { Botuser } = require("../../db.js");
+const { Client } = require("../../db.js");
+const { Pc } = require("../../db.js");
 
 const router = Router();
 
-router.get('/', async (req, res) => {
-
-    const username = process.env.CLOUDINARY_API_KEY
-    const password = process.env.CLOUDINARY_API_SECRET
-    const basicAuth = `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`;
-
-try {
-  const response = await axios.get(`https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/image`, {
-    headers: {
-      Authorization: basicAuth,
-    },
-  });
-
-  console.log(response.data);
-
-  return res.json(response.data)
+// Ruta GET para pruebas
+router.get("/", async (req, res) => {
+  try {
+    return res.status(200).json("Testing");
   } catch (error) {
-    console.error('Error al cargar las imágenes de Cloudinary:', error);
+    console.error(error);
+    res.status(500).json({ message: "Error interno del servidor." });
   }
-})
+});
+
+router.post("/bottickets", async (req, res) => {
+  try {
+    const bottickets = await Botticket.findAll();
+
+    return res.status(200).json(bottickets);
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
+});
+
+router.post("/botusers", async (req, res) => {
+  try {
+    const botusers = await Botuser.findAll();
+
+    return res.status(200).json(botusers);
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
+});
+
+router.post("/clients", async (req, res) => {
+  try {
+    const clients = await Client.findAll();
+
+    return res.status(200).json(clients);
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
+});
+
+router.post("/pcs", async (req, res) => {
+  try {
+    const pcs = await Pc.findAll();
+
+    return res.status(200).json(pcs);
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
+});
 
 module.exports = router;
